54089973a68a04767447711d12f3ec84862da74f
Torkel <182918@stud.hvl.no>
2020-02-13 12:53:07 +0100

    plzwork

---
+++ solsystemet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSim;
+
+class Astronomy
+{
+    public static void Main()
+    {
+        allSpaceObjects planets = new allSpaceObjects();
+        planets.allPlanets();
+        planets.allMoons();
+        List<SpaceObject> solarSystem = planets.objectList;
+
+        foreach (SpaceObject obj in solarSystem) { 
+            obj.Draw();
+            if (obj.children.Count != 0) {
+                Console.WriteLine(" Moons: ");
+                foreach (SpaceObject child in obj.children)
+                {
+                    child.Draw();
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ wpfSolarSystem/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SpaceSim;
+
+namespace wpfSolarSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate void delg(int time);
+        private System.Windows.Threading.DispatcherTimer t;
+        private allSpaceObjects allObjects;
+        List<SpaceObject> list;
+        int time = 0; 
+        public event delg moveIt;
+        public MainWindow()
+        {
+            InitializeComponent();
+            allObjects.allPlanets();
+            //allObjects.allMoons();
+            list = allObjects.objectList;
+            t = new System.Windows.Threading.DispatcherTimer(); 
+            t.Interval = new TimeSpan(200000); 
+            t.Tick += t_Tick; 
+            t.Start();
+            subscribePlanets();
+        }
+        void t_Tick(object sender, EventArgs e) { moveIt(time++); }
+
+        void subscribePlanets() {
+            for (int i = 0; i < list.Count(); i++) {
+                moveIt += list[i].calcPos;
+            }
+        }
+    }
+}
